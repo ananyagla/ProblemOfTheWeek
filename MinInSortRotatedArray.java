dcfdc0a4a32d5535b2a89a9dae1371f064eeb3c0
Ananya Chaurasia <ananyachaurasia74@gmail.com>
2025-08-10 08:02:30 +0530

    Commit changes

--- MinInSortRotatedArray.java
+++ MinInSortRotatedArray.java
@@ -1,3 +1,38 @@
-public class MinInSortRotatedArray{
+import java.util.Scanner;
+
+public class MinInSortRotatedArray {
     
+    // Method to find minimum in sorted rotated array
+    public static int findMin(int[] arr) {
+        int low = 0;
+        int high = arr.length - 1;
+        
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] > arr[high]) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return arr[low];
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements in array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int minValue = findMin(arr);
+        System.out.println("Minimum value in the array is: " + minValue);
+
+        sc.close();
+    }
 }
